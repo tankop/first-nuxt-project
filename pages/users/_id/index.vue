e332fc4fbe2c68bd75419da874260790486c5618
tankop <peter.tanko@tancode.hu>
2020-03-14 19:15:06 +0100

    dynamic route

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>A sigle user, with ID: {{$route.params.id}}</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/users/_id/more-details.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>More details</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
