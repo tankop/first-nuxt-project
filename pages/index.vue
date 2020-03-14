38c238e97e75d0eaf49258abdecf5fc36dfc305a
tankop <peter.tanko@tancode.hu>
2020-03-14 19:25:18 +0100

    adding links & navigating around

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Hello
-      </h1>
-      <h2 class="subtitle">
-        My luminous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <nuxt-link to="/users">Users</nuxt-link>
   </div>
 </template>
 
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>This is users page</h1>
+  <div>
+    <input type="text" v-model="userId">
+    <button @click="onLoadUser">Load user</button>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      userId: ''
+    }
+  },
+  methods: {
+    onLoadUser(){
+      this.$router.push('/users/' + this.userId)
+
+    }
+  }
 
 }
 </script>
